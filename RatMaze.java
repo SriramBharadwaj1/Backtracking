dd83fe966c174e1fe2633a44ea88d3c21420b991
Sriram Bharadwaj <92371562+SriramBharadwaj1@users.noreply.github.com>
2022-09-11 17:13:01 +0530

    Update RatMaze.java

--- RatMaze.java
+++ RatMaze.java
@@ -1,18 +1,27 @@
+/*
+	the aim is to find a path for a mouse to solve a maze(if one exists). a block is indicated by 0 in maze and a clear way with 1. the mouse begins at 
+	box (0,0), and can take any of the two moves: 1) move forward, i.e from left to right, or 2) move downward
+*/
+
+
+
+
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 class RatMaze
 {
-	static boolean issafe(int n, int x, int y,int[][] maze,int[][] sol)
+	static boolean issafe(int n, int x, int y,int[][] maze,int[][] sol)                           //check if mouse can move to this square
 	{
 		return( x<n && y<n && x>=0 && y>=0 && maze[x][y]==1);
 	}
 
 
-	static boolean path(int n,int x, int y, int [] xmoves, int[] ymoves, int[][] maze, int[][] sol)
+	static boolean path(int n,int x, int y, int [] xmoves, int[] ymoves, int[][] maze, int[][] sol)          //pathfinder function
 	{
 
-		if(x==n-1 && y==n-1)
+		if(x==n-1 && y==n-1)                                                                             //endpoint has been reached
 		{	
 			sol[x][y]=1;
 			for(int [] i:sol)
@@ -24,16 +33,16 @@ class RatMaze
 
 	
 				
-		if(issafe(n,x,y,maze))
+		if(issafe(n,x,y,maze))                  
 		{
 			sol[x][y]=1;
 			
-			if(path(n,x+1,y,xmoves,ymoves,maze,sol))
+			if(path(n,x+1,y,xmoves,ymoves,maze,sol))                                            //check moving forward
 			{
 				return true;
 			}
 			
-			if(path(n,x,y+1,xmoves,ymoves,maze,sol))
+			if(path(n,x,y+1,xmoves,ymoves,maze,sol))                                            //cheeck moving downward
 			{
 				return true;
 			}
